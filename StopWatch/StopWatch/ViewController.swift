4ca822ba6b729f9f0165d9e55a0ab1e679a9ef42
WindTalkerWTF <181138630@qq.com>
2017-08-10 22:17:12 +0800

    初始化项目

---
+++ StopWatch/StopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by WindTalker on 2017/8/10.
+//  Copyright © 2017年 WindTalker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
